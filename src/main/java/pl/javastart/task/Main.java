9dcf549329fb725c7b3af53ba5968e71ffdf8dba
kamilkubiak2210 <kamil.kubiak4@outlook.com>
2023-06-09 14:58:15 +0200

    Poprawka.

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,7 +1,12 @@
 package pl.javastart.task;
 
-import java.time.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -12,26 +17,23 @@ public class Main {
     }
 
     public void run(Scanner scanner) {
-        printGeneralInfo(scanner);
+        List<String> allFormats = createAllFormats();
+        LocalDateTime dateTime = getLocalDateTime(scanner, allFormats);
+        if (dateTime != null) {
+            printInfoAboutZonedDateTime(dateTime);
+        } else {
+            System.out.println("Nieobsługiwany format");
+        }
     }
 
-    public static void printGeneralInfo(Scanner scanner) {
-        System.out.println("Podaj datę:");
-        String userInput = scanner.nextLine();
-        int dotUnicode = 46;
+    public static List<String> createAllFormats() {
+        List<String> formatterList = new LinkedList<>();
+        formatterList.add("yyyy-MM-dd HH:mm:ss");
+        formatterList.add("dd.MM.yyyy HH:mm:ss");
+        return formatterList;
+    }
 
-        LocalDateTime dateTime;
-        if (userInput.length() == 10) {
-            LocalDate date = LocalDate.parse(userInput, DateTimeFormatter.ISO_LOCAL_DATE);
-            dateTime = date.atStartOfDay();
-        } else if (userInput.charAt(2) == (char) dotUnicode) {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
-            dateTime = LocalDateTime.parse(userInput, formatter);
-        } else {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            dateTime = LocalDateTime.parse(userInput, formatter);
-        }
-
+    public static void printInfoAboutZonedDateTime(LocalDateTime dateTime) {
         ZonedDateTime localZone = dateTime.atZone(ZoneId.systemDefault());
         ZonedDateTime utcZone = localZone.withZoneSameInstant(ZoneId.of("UTC"));
         ZonedDateTime londonZone = localZone.withZoneSameInstant(ZoneId.of("Europe/London"));
@@ -44,5 +46,36 @@ public class Main {
         System.out.println("Londyn: " + londonZone.format(outputFormatter));
         System.out.println("Los Angeles: " + losAngelesZone.format(outputFormatter));
         System.out.println("Sydney: " + sydneyZone.format(outputFormatter));
+    }
+
+    private static LocalDateTime getLocalDateTime(Scanner scanner, List<String> allFormats) {
+        System.out.println("Podaj datę:");
+        String userInput = scanner.nextLine();
+        LocalDateTime dateTime = null;
+
+        int dotUnicode = 46;
+        if (userInput.length() == 10) {
+            LocalDate date = LocalDate.parse(userInput, DateTimeFormatter.ISO_LOCAL_DATE);
+            dateTime = date.atStartOfDay();
+            return dateTime;
+        } else {
+            if (userInput.length() > 2) {
+                for (String allFormat : allFormats) {
+                    if (userInput.charAt(2) == (char) dotUnicode && allFormat.charAt(2) == (char) dotUnicode) {
+                        dateTime = getLocalDateTime(allFormat, userInput);
+                        return dateTime;
+                    } else if ((userInput.length() == 19) && (allFormat.length() == 19) && userInput.charAt(2) != (char) dotUnicode) {
+                        dateTime = getLocalDateTime(allFormat, userInput);
+                        return dateTime;
+                    }
+                }
+            }
+        }
+        return dateTime;
+    }
+
+    private static LocalDateTime getLocalDateTime(String pattern, String userInput) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        return LocalDateTime.parse(userInput, formatter);
     }
 }
